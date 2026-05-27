346bf6ca66e4507fb407760d3c11c51f6ceb7682
owennhorton <owen.n.horton@lawrence.edu>
2026-05-27 04:15:56 +0000

    Added documentation for toy_data, added a readme, added a vignette, build a website using pkgdown

---
+++ R/toy_data.R
@@ -0,0 +1,12 @@
+#' toy_dataset for reportr purposes
+#'
+#' A 3x15 toy dataset that allows the user to familiarize themselves with lin_out, log_out, and kick_out.
+#'
+#' @name {toy_data}
+#' @format A data frame with 15 rows and 3 variables:
+#' \describe{
+#'   \item{x1}{A numerical column that can operate as the sole predictor required for lin_out and log_out}
+#'   \item{y1}{A numerical column that can operate as a numerical response required for lin_out}
+#'   \item{y2}{A categorical column that can operate as a binary response required for log_out}
+#' }
+"toy_data"
